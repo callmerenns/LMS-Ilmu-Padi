1f9a7ecffe257c062aa4db44584301600a3d2d9a
renns19 <mahasiswaedan19@gmail.com>
2024-05-30 11:13:16 +0700

    [UPDATE] tag struct on field email

--- entity/user.go
+++ entity/user.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	ID                 string         `json:"id"`
 	Name               string         `json:"name"`
-	Email              string         `json:"email"`
+	Email              string         `json:"email,omitempty"`
 	Password           string         `json:"password"`
 	Role               string         `json:"role"`
 	SubscriptionStatus string         `json:"subscription_status"`
