1d4e45512b5813b67b9863132844d077ef5afde7
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2024-06-01 23:10:43 +0700

    chore: fix typo

--- assets/ilmu_padi_db_DML.sql
+++ assets/ilmu_padi_db_DML.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (Name, Email, Password, Role, SubscriptionStatus) VALUES
+INSERT INTO users (name, email, password, role) VALUES
 ('Al Tsaqif Nugraha Ahmad', 'altsaqifnugraha19@gmail.com', '2wsx1qaz', 'admin', 'active'),
 ('Angga Lesmana', 'anggalesmana131@gmail.com', '2wsx1qaz', 'normal-user', 'inactive'),
 ('Sien Khumaen Damarwendha', 'sienkhumaen@gmail.com', '2wsx1qaz', 'premium-user', 'active');
 
-INSERT INTO courses (Title, Description, ContentURL, AuthorID, IsFree) VALUES
+INSERT INTO courses (title, description, contentURL, AuthorID, IsFree) VALUES
 ('Introduction to PostgreSQL', 'A beginner course on PostgreSQL', 'http://example.com/course1', 
     (SELECT UserID FROM users WHERE Email = 'altsaqifnugraha19@gmail.com'), TRUE),
 ('Advanced PostgreSQL', 'An advanced course on PostgreSQL', 'http://example.com/course2', 
@@ -15,16 +15,3 @@ INSERT INTO subscriptions (UserID, StartDate, EndDate, IsActive) VALUES
 ((SELECT UserID FROM users WHERE Email = 'altsaqifnugraha19@gmail.com'), '2024-01-01', '2025-01-01', TRUE),
 ((SELECT UserID FROM users WHERE Email = 'anggalesmana131@gmail.com'), '2024-01-01', '2024-06-01', FALSE),
 ((SELECT UserID FROM users WHERE Email = 'sienkhumaen@gmail.com'), '2024-01-01', '2025-01-01', TRUE);
-
-INSERT INTO ads (Content, CourseID) VALUES
-('Check out our new course on PostgreSQL!', 
-    (SELECT CourseID FROM courses WHERE Title = 'Introduction to PostgreSQL')),
-('Join our advanced PostgreSQL course now!', 
-    (SELECT CourseID FROM courses WHERE Title = 'Advanced PostgreSQL')),
-('Learn how to tune PostgreSQL with our new course!', 
-    (SELECT CourseID FROM courses WHERE Title = 'PostgreSQL Performance Tuning'));
-
-INSERT INTO password_resets (UserID, Token, Expiration) VALUES
-((SELECT UserID FROM users WHERE Email = 'altsaqifnugraha19@gmail.com'), 'token123', '2024-06-01 00:00:00'),
-((SELECT UserID FROM users WHERE Email = 'anggalesmana131@gmail.com'), 'token456', '2024-06-01 00:00:00'),
-((SELECT UserID FROM users WHERE Email = 'sienkhumaen@gmail.com'), 'token789', '2024-06-01 00:00:00');
--- delivery/controller/user_controller.go
+++ delivery/controller/user_controller.go
@@ -13,7 +13,7 @@ type userCtrl struct {
 	userUsecase usecase.IUserUsecase
 }
 
-func (ct *userCtrl) RegisterCtrl(c *gin.Context) {
+func (ct *userCtrl) CreateCtrl(c *gin.Context) {
 	var payload entity.User
 
 	if err := c.ShouldBindJSON(&payload); err != nil {
@@ -30,10 +30,8 @@ func (ct *userCtrl) RegisterCtrl(c *gin.Context) {
 	dto.SendSingleResponse(c, nil, http.StatusCreated, "Created")
 }
 
-func (ct *userCtrl) LoginCtrl(c *gin.Context) {}
-
 func (ct *userCtrl) Routing(r *gin.RouterGroup) {
-	r.POST("/users", ct.RegisterCtrl)
+	r.POST("/users", ct.CreateCtrl)
 }
 
 func NewUserCtrl(userUsecase usecase.IUserUsecase) *userCtrl {
