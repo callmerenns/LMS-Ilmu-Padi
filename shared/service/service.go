31ee4cd587e010bd0dda9f640ebe42fa1bf6355e
renns19 <mahasiswaedan19@gmail.com>
2024-06-04 15:26:53 +0700

    feat(payment) fix service payment

--- shared/service/service.go
+++ shared/service/service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"fmt"
+	"os"
 	"strconv"
 
+	"github.com/joho/godotenv"
 	"github.com/kelompok-2/ilmu-padi/entity"
 	"github.com/veritrans/go-midtrans"
 )
@@ -19,9 +22,21 @@ func NewService() *service {
 }
 
 func (s *service) GetPaymentURL(transaction Transaction, user entity.User) (string, error) {
+	if err := godotenv.Load(); err != nil {
+		return "", fmt.Errorf("missing env file %v", err.Error())
+	}
+
+	var (
+		ClientKey string
+		ServerKey string
+	)
+
+	ClientKey = os.Getenv("CLIENT_KEY")
+	ServerKey = os.Getenv("SERVER_KEY")
+
 	midclient := midtrans.NewClient()
-	midclient.ServerKey = ""
-	midclient.ClientKey = ""
+	midclient.ServerKey = ClientKey
+	midclient.ClientKey = ServerKey
 	midclient.APIEnvType = midtrans.Sandbox
 
 	snapGateway := midtrans.SnapGateway{
